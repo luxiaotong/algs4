f78483071b8ea5786c6df7d7a1016e1cf266c1d7
luxiaotong <luxiaotong713@sina.com>
2014-10-05 21:08:39 +0800

    Add sample and iterator function

--- RandomizedQueue.java
+++ RandomizedQueue.java
@@ -1,10 +1,8 @@
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class RandomizedQueue<Item> /*implements Iterable<Item>*/ {
+public class RandomizedQueue<Item> implements Iterable<Item> {
 	/*
-   public Item sample()                     // return (but do not delete) a random item
-   public Iterator<Item> iterator()         // return an independent iterator over items in random order
 	*/
 	private Node first 	= null;
 	private Node last 	= null;
@@ -70,6 +68,24 @@ public class RandomizedQueue<Item> /*implements Iterable<Item>*/ {
         return item;
 	}
 
+   	public Item sample()                     // return (but do not delete) a random item
+	{
+		if (isEmpty()) throw new NoSuchElementException("Queue underflow");
+		
+		int n 		= size();
+		int rand 	= StdRandom.uniform(n);
+		int i 		= 0;
+		Node pop 	= first;
+		Node prev 	= first;
+
+		for ( i = 0; i < rand; i ++ ) {
+			pop 	= pop.next;
+		}
+		Item item = pop.item;
+        return item;
+
+	}
+
 	public void check()
 	{
 		Node tmp;
@@ -81,9 +97,34 @@ public class RandomizedQueue<Item> /*implements Iterable<Item>*/ {
 			StdOut.println(tmp.item);
 		}
 		
+		/*
 		StdOut.println("first: " + first.item);
 		StdOut.println("last : " + last.item);
 		StdOut.println("size : " + size());
+		*/
+	}
+   
+	public Iterator<Item> iterator()         // return an independent iterator over items in random order
+	{
+		return new ListIterator();
+	}
+
+	private class ListIterator implements Iterator<Item>
+	{
+		private Node current = first;
+		
+		public void remove() { throw new UnsupportedOperationException("Not support"); }
+
+		public boolean hasNext() { return current != null; }
+		
+		public Item next()
+		{
+			if (!hasNext()) throw new NoSuchElementException();
+
+			Item item 	= current.item;
+			current 	= current.next;
+			return item;
+		}
 	}
    	
 	public static void main(String[] args)   // unit testing
@@ -92,15 +133,14 @@ public class RandomizedQueue<Item> /*implements Iterable<Item>*/ {
         while (!StdIn.isEmpty()) {
             String item = StdIn.readString();
             if (!item.equals("-")) rq.enqueue(item);
-            else if (!rq.isEmpty()) StdOut.println(rq.dequeue());
+            //else if (!rq.isEmpty()) StdOut.println(rq.dequeue());
+            //else if (!rq.isEmpty()) StdOut.println(rq.sample());
+			else break;
 			
-			rq.check();
+			//rq.check();
         }
-		/*
-		Iterator<String> it = d.iterator();
-		for(String idq : d) {
-			StdOut.println(idq);}
-		*/
+		for(String str : rq)
+			StdOut.println(str);
         StdOut.println("(" + rq.size() + " left on deque)");
 	}
 
